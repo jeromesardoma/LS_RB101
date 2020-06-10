7f49ff1f8e285a7bdfb73fb010b0265d076b42c1
Jerome Sardoma <jeromesardoma@gmail.com>
2020-06-10 09:37:12 -0700

    fixed tabs

--- calculator.rb
+++ calculator.rb
@@ -1,33 +1,33 @@
 def is_valid_number? number_string
-	number_string.to_i.to_s == number_string
+  number_string.to_i.to_s == number_string
 end
 
 loop do
 
-	puts "Welcome to calculator!  Enter a number, or 'q' to quit: "
-	entry_1 = gets.chomp
-	break if entry_1.downcase == 'q'
+  puts "Welcome to calculator!  Enter a number, or 'q' to quit: "
+  entry_1 = gets.chomp
+  break if entry_1.downcase == 'q'
 
-	puts "Enter the next number: "
-	entry_2 = gets.chomp
+  puts "Enter the next number: "
+  entry_2 = gets.chomp
 
-	if [ entry_1, entry_2 ].any?{ |e| is_valid_number? e } == false 
-		puts "You entered an invalid entry, try again."
-		next
-	end 
+  if [ entry_1, entry_2 ].any?{ |e| is_valid_number? e } == false 
+    puts "You entered an invalid entry, try again."
+    next
+  end 
 
-	entry_1 = entry_1.to_i
-	entry_2 = entry_2.to_i
+  entry_1 = entry_1.to_i
+  entry_2 = entry_2.to_i
 
-	puts "What operation would you like to perform? (add / subtract / multiply / divide )"
-	operation = gets.chomp
+  puts "What operation would you like to perform? (add / subtract / multiply / divide )"
+  operation = gets.chomp
 
-	case operation
-		when "add" 			then puts "#{entry_1} + #{entry_2} = #{ entry_1 + entry_2 }"
-		when "subtract" then puts "#{entry_1} - #{entry_2} = #{ entry_1 - entry_2 }"
-		when "multiply" then puts "#{entry_1} * #{entry_2} = #{ entry_1 * entry_2 }"
-		when "divide" 	then puts "#{entry_1} / #{entry_2} = #{ entry_1.to_f / entry_2.to_f }"
-		else 								 puts "You entered an invalid operation, try again."
-	end
+  case operation
+    when "add"      then puts "#{entry_1} + #{entry_2} = #{ entry_1 + entry_2 }"
+    when "subtract" then puts "#{entry_1} - #{entry_2} = #{ entry_1 - entry_2 }"
+    when "multiply" then puts "#{entry_1} * #{entry_2} = #{ entry_1 * entry_2 }"
+    when "divide"   then puts "#{entry_1} / #{entry_2} = #{ entry_1.to_f / entry_2.to_f }"
+    else                   puts "You entered an invalid operation, try again."
+  end
 
 end 
