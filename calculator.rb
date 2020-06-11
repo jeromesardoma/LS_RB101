8b7b8468aeb2b5439cdb19a8afad04baca4671da
Jerome Sardoma <jeromesardoma@gmail.com>
2020-06-10 18:17:25 -0700

    finished basic calculator

--- calculator.rb
+++ calculator.rb
@@ -1,33 +1,74 @@
-def is_valid_number? number_string
-  number_string.to_i.to_s == number_string
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def is_integer?(string)
+  string.to_i.to_s == string
+end
+
+def is_float?(string)
+  string.to_f.to_s == string
+end
+
+def valid_number?(number_string)
+  is_integer?(number_string) || is_float?(number_string)
 end
 
 loop do
+  entry1 = nil
+  entry2 = nil
 
-  puts "Welcome to calculator!  Enter a number, or 'q' to quit: "
-  entry_1 = gets.chomp
-  break if entry_1.downcase == 'q'
+  loop do
+    prompt MESSAGES['welcome']
+    entry1 = gets.chomp
 
-  puts "Enter the next number: "
-  entry_2 = gets.chomp
+    prompt MESSAGES['welcome2']
+    entry2 = gets.chomp
 
-  if [ entry_1, entry_2 ].any?{ |e| is_valid_number? e } == false 
-    puts "You entered an invalid entry, try again."
-    next
-  end 
+    if [entry1, entry2].any? { |e| valid_number? e } == false
+      prompt MESSAGES['invalid_entries']
+      next
+    end
 
-  entry_1 = entry_1.to_i
-  entry_2 = entry_2.to_i
-
-  puts "What operation would you like to perform? (add / subtract / multiply / divide )"
-  operation = gets.chomp
-
-  case operation
-    when "add"      then puts "#{entry_1} + #{entry_2} = #{ entry_1 + entry_2 }"
-    when "subtract" then puts "#{entry_1} - #{entry_2} = #{ entry_1 - entry_2 }"
-    when "multiply" then puts "#{entry_1} * #{entry_2} = #{ entry_1 * entry_2 }"
-    when "divide"   then puts "#{entry_1} / #{entry_2} = #{ entry_1.to_f / entry_2.to_f }"
-    else                   puts "You entered an invalid operation, try again."
+    break
   end
 
-end 
+  entry1 = entry1.to_f
+  entry2 = entry2.to_f
+
+  loop do
+    operator_prompt = <<-MSG
+    What operation would you like to perform?
+      1) add
+      2) subtract
+      3) multiply
+      4) divide
+    MSG
+
+    prompt operator_prompt
+    operation = gets.chomp.to_i
+
+    case operation
+    when 1 then prompt "#{entry1} + #{entry2} = #{entry1 + entry2}"
+    when 2 then prompt "#{entry1} - #{entry2} = #{entry1 - entry2}"
+    when 3 then prompt "#{entry1} * #{entry2} = #{entry1 * entry2}"
+    when 4 then prompt "#{entry1} / #{entry2} = #{entry1.to_f / entry2.to_f}"
+    else
+      prompt MESSAGES['invalid_operation']
+      next
+    end
+
+    break
+  end
+
+  prompt MESSAGES['perform_again']
+  response = gets.chomp
+  next if response.downcase.start_with? 'y'
+  if response.downcase.start_with? 'q'
+    prompt MESSAGES['exit_message']
+    break
+  end
+end
